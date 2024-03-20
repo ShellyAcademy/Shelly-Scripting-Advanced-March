37c8ff9e3a384c503934e17df5ed76238a4b39c5
DenisB22 <bechiragich97@gmail.com>
2024-03-21 00:38:00 +0200

    Demo

--- demo/07.sensorWithScript.js
+++
@@ -1,7 +0,0 @@
-const SHELLY_PLUS_2PM_IP = "172.20.10.4";
-// let url = 'http://' + SHELLY_PLUS_2PM_IP + '/rpc/SensorAddon.AddPeripheral?type="ds18b20"&attrs={"cid":100, "addr": "40:70:103:129:227:228:60:191"}';
-let url = 'http://' + SHELLY_PLUS_2PM_IP + '/rpc/SensorAddon.AddPeripheral?type="ds18b20"&attrs={"cid":100,%20"addr":%20"40:70:103:129:227:228:60:191"}';
-
-Shelly.call("HTTP.GET", {url: url}, function(result) {
-  console.log(result);
-});
